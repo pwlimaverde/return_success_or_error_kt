77de363846578632d68f493e1318cbbab66b06ee
pwlimaverde <pwlimaverde@gmail.com>
2024-08-29 22:31:38 -0300

    teste finish gitflow

--- src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/Main.kt
+++ src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/Main.kt
@@ -1,8 +1,8 @@
 package com.pwlimaverde.return_success_or_error_kt
 
 fun testeCalck(): Int {
-    var t = 3
-    var t2 = 3
+    var t = 4
+    var t2 = 4
     println("teste ${t+t2}")
     return t+t2
 }
