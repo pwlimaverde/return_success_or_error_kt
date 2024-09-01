4ac362c929b7b921ff5776292f3b74e65fafc25d
pwlimaverde <pwlimaverde@gmail.com>
2024-09-01 15:18:07 -0300

    correção de import

--- src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/usecase/Repository.kt
+++ src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/usecase/Repository.kt
@@ -4,7 +4,6 @@ import com.pwlimaverde.return_success_or_error_kt.core.ErrorReturn
 import com.pwlimaverde.return_success_or_error_kt.core.ReturnSuccessOrError
 import com.pwlimaverde.return_success_or_error_kt.core.SuccessReturn
 import com.pwlimaverde.return_success_or_error_kt.datasource.DataSource
-import com.pwlimaverde.return_success_or_error_kt.error.AppError
 import com.pwlimaverde.return_success_or_error_kt.parameters.ParametersReturnResult
 
 sealed class Repository<TypeDataSource, Parameters : ParametersReturnResult>(
