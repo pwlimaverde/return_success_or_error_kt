053955db771024afc18cc0886aeab95806ab89dc
pwlimaverde <pwlimaverde@gmail.com>
2024-09-04 22:37:34 -0300

    correção imports

--- src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/usecase/UseCaseBase.kt
+++ src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/usecase/UseCaseBase.kt
@@ -2,9 +2,7 @@ package com.pwlimaverde.return_success_or_error_kt.usecase
 
 import com.pwlimaverde.return_success_or_error_kt.core.ReturnSuccessOrError
 import com.pwlimaverde.return_success_or_error_kt.parameters.ParametersReturnResult
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 
--- src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/usecase/UseCaseCallData.kt
+++ src/main/kotlin/com/pwlimaverde/return_success_or_error_kt/usecase/UseCaseCallData.kt
@@ -1,9 +1,7 @@
 package com.pwlimaverde.return_success_or_error_kt.usecase
 
-import com.pwlimaverde.return_success_or_error_kt.core.ReturnSuccessOrError
 import com.pwlimaverde.return_success_or_error_kt.datasource.DataSource
 import com.pwlimaverde.return_success_or_error_kt.parameters.ParametersReturnResult
-import kotlinx.coroutines.suspendCancellableCoroutine
 
 abstract class UseCaseCallData<TypeUseCase, TypeDataSource, Parameters : ParametersReturnResult>(
     dataSource: DataSource<TypeDataSource, Parameters>,
